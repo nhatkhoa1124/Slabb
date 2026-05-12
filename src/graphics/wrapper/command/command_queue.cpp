25c8c479192a42d7fb1357b7bd1d929f8edbccf2
Khoa Tran <nhatkhoa1124@gmail.com>
2026-05-13 02:51:56 +0700

    feat: Add command queue creation

--- src/graphics/wrapper/command/command_queue.cpp
+++ src/graphics/wrapper/command/command_queue.cpp
@@ -17,5 +17,14 @@ namespace slabb::graphics::wrapper::command
 			spdlog::error("ERROR: No device detected while creating Command Queue");
 			throw std::runtime_error("NULLPTR ERROR");
 		}
+		D3D12_COMMAND_QUEUE_DESC cmdQueueDesc =
+		{
+			.Type = D3D12_COMMAND_LIST_TYPE_DIRECT,
+			.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL,
+			.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE,
+			.NodeMask = 0
+		};
+		spdlog::trace("Creating Command Queue");
+		SLABB_CHECK(device->CreateCommandQueue(&cmdQueueDesc, IID_PPV_ARGS(&m_cmd_queue)));
 	}
 }
